58a70d171387e07561e717b584a78434e31701be
Oleksii Fedorkan <alex.fedorkan@gmail.com>
2021-04-03 13:16:44 +0300

    Add swagger, endpoints: register, login, logout, docs.

---
+++ controllers/auth.js
@@ -0,0 +1,88 @@
+const jwt = require("jsonwebtoken");
+const Users = require("../model/users");
+const { HttpCode } = require("../helpers/constants");
+const User = require("../model/schemas/user");
+
+require("dotenv").config();
+const SECRET_KEY = process.env.JWT_SECRET;
+
+const register = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+
+    const user = await Users.findByEmail(email);
+
+    if (user) {
+      return res.status(HttpCode.CONFLICT).json({
+        status: "error",
+        code: HttpCode.CONFLICT,
+        data: "Conflict",
+        message: "Email in use",
+      });
+    }
+
+    const newUser = await User.create(req.body);
+
+    return res.status(HttpCode.CREATED).json({
+      status: "success",
+      code: HttpCode.CREATED,
+      data: {
+        user: {
+          name: newUser.name,
+          email: newUser.email,
+        },
+      },
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await Users.findByEmail(email);
+
+    if (!user || !(await user.validPassword(password))) {
+      return res.status(HttpCode.UNAUTHORIZED).json({
+        status: "error",
+        code: HttpCode.UNAUTHORIZED,
+        data: "Unauthorized",
+        message: "Email or password is wrong",
+      });
+    }
+
+    const id = user._id;
+
+    const payload = { id };
+
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "2h" });
+
+    await Users.updateToken(id, token);
+
+    return res.status(HttpCode.OK).json({
+      status: "success",
+      code: HttpCode.OK,
+      data: {
+        token,
+        user: {
+          name: user.name,
+          email: user.email,
+        },
+      },
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+const logout = async (req, res, next) => {
+  const userID = req.user.id;
+
+  await Users.updateToken(userID, null);
+
+  return res.status(HttpCode.NO_CONTENT).json({});
+};
+
+module.exports = { register, login, logout };
---
+++ routes/auth/validation.js
@@ -0,0 +1,39 @@
+const Joi = require("joi");
+const { HttpCode } = require("../../helpers/constants");
+
+const schemaRegister = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  password: Joi.string().min(6).max(16).required(),
+});
+
+const schemaLogin = Joi.object({
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  password: Joi.string().min(6).max(16).required(),
+});
+
+const validate = (schema, object, next) => {
+  const { error } = schema.validate(object);
+
+  if (error) {
+    const [{ message }] = error.details;
+    return next({
+      status: HttpCode.BAD_REQUEST,
+      message: `Filed: ${message.replace(/"/g, "")}`,
+    });
+  }
+
+  next();
+};
+
+module.exports.register = (req, res, next) => {
+  return validate(schemaRegister, req.body, next);
+};
+
+module.exports.login = (req, res, next) => {
+  return validate(schemaLogin, req.body, next);
+};
