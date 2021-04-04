374144880a40f0a0da15adb218c80942f1d0686b
Ivan <vitanlhouse@gmail.com>
2021-04-04 16:45:49 +0300

    Додає набори питань та заготовку для створення енд-пойнта

---
+++ controllers/questions.js
@@ -0,0 +1,3 @@
+const { HttpCode } = require("../helpers/constants");
+
+module.exports = {};
--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../../controllers/auth");
+const questionsController = require("../../controllers/questions");
 const { register, login } = require("./validation");
 const guard = require("../../helpers/guard");
 
@@ -9,5 +10,7 @@ router.post("/register", register, authController.register);
 router.post("/login", login, authController.login);
 
 router.post("/logout", guard, authController.logout);
+
+router.post("/tech-questions");
 
 module.exports = router;
