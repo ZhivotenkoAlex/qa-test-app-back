63d82ac7a6bf346103a8cab78740fa6140e64e72
Oleksii Fedorkan <alex.fedorkan@gmail.com>
2021-04-07 22:59:09 +0300

    Add gmail authentication

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../../controllers/auth");
 const { register, login } = require("./validation");
-const guard = require("../../helpers/guard"); 
+const guard = require("../../helpers/guard");
 
 router.post("/register", register, authController.register);
 
 router.post("/login", login, authController.login);
+
+router.get("/google", authController.googleAuth);
+
+router.get("/google-redirect", authController.googleRedirect);
 
 router.post("/logout", guard, authController.logout);
 
