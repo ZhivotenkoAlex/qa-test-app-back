1b8715dd4e0965f783cbb4f15a46c542159818c2
Oleksii Fedorkan <alex.fedorkan@gmail.com>
2021-04-16 01:50:45 +0300

    Fix questions routes.

--- routes/test-tech/index.js
+++ routes/test-tech/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const guard = require("../../helpers/guard");
 const { listQuestionsTech } = require("../../model/questions-tech");
 
 const getRandomIntInclusive = (min, max) => {
@@ -9,7 +8,7 @@ const getRandomIntInclusive = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-router.get("/", guard, async (_req, res, next) => {
+router.get("/", async (_req, res, next) => {
   try {
     const list = await listQuestionsTech();
     const arrInt = [];
@@ -34,7 +33,7 @@ router.get("/", guard, async (_req, res, next) => {
   }
 });
 
-router.post("/right-answers", guard, async (req, res, next) => {
+router.post("/right-answers", async (req, res, next) => {
   try {
     let rightAnswer = 0;
     const list = await listQuestionsTech();
